f41c6342ae7dc1806119ae1043a78a2d47a7d7ac
Lagoria Luciano <luchicapo@gmail.com>
2025-10-31 14:42:34 -0300

    Falla tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
 import ServiceCalculator from './pages/ServiceCalculator'
 
 function App() {
   return (
-    <>
-      <div className='flex justify-center items-center h-screen'>
-        <ServiceCalculator />
-      </div>
-    </>
+    // Aplicamos las clases de Tailwind para el fondo oscuro,
+    // el centrado y el padding (espaciado).
+    <div className="flex justify-center items-start min-h-screen w-full bg-gray-900 text-gray-200 p-4 md:p-8">
+      <ServiceCalculator />
+    </div>
   )
 }
 
