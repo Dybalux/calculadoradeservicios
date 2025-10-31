4ee2b62dae0e168203f6703bdf6cab0d100ae01c
Lagoria Luciano <luchicapo@gmail.com>
2025-10-31 15:15:14 -0300

    Ya importanmos correctamente tailwind, era una incompatibilidad entre las versiones 3 y 4

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/postcss' // <-- 1. Importa tailwindcss
-import autoprefixer from 'autoprefixer'       // <-- 2. Importa autoprefixer
+import tailwindcss from '@tailwindcss/vite' // <-- ¡Ahora sí va a encontrar este paquete!
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss,  // <-- 3. Pásalo como VARIABLE (sin comillas)
-        autoprefixer, // <-- 4. Pásalo como VARIABLE (sin comillas)
-      ],
-    },
-  },
+  plugins: [
+    react(),
+    tailwindcss(), // <-- Y ahora sí lo podemos usar como plugin
+  ],
 })
