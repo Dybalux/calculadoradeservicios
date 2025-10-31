f743b333541a78b48f46555a54b578e9b18fa0a6
Lagoria Luciano <luchicapo@gmail.com>
2025-10-31 15:13:03 -0300

    Seguimos con errores al configrar tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/postcss' // <-- 1. Importa tailwindcss
+import autoprefixer from 'autoprefixer'       // <-- 2. Importa autoprefixer
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,  // <-- 3. Pásalo como VARIABLE (sin comillas)
+        autoprefixer, // <-- 4. Pásalo como VARIABLE (sin comillas)
+      ],
+    },
+  },
 })
