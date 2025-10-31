33442938c43d497dff88944ad44cd97d77033970
Lagoria Luciano <luchicapo@gmail.com>
2025-10-31 15:41:28 -0300

    Configuracion para github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite' // <-- ¡Ahora sí va a encontrar es
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/calculadoradeservicios/",
   plugins: [
     react(),
     tailwindcss(), // <-- Y ahora sí lo podemos usar como plugin
